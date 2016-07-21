d23929a8bed0460baed48b90bd5dc0e3188de391
Andrew Chung <andrewchungg@gmail.com>
2016-07-21 01:15:07 -0400

    added todo list with working node api, added styling to index

--- app.js
+++ app.js
@@ -1,14 +1,74 @@
-var app = require('express')();
+var express = require('express')
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var mongoose = require('mongoose');                     
+var morgan = require('morgan');           
+var bodyParser = require('body-parser');  
+var methodOverride = require('method-override');
+var config = require('./superSecretDatabaseConfig');
+
+mongoose.connect('mongodb://' + config.mongoUsername + ':' + config.mongoPassword + config.database);
+
+app.use(express.static(__dirname + '/public'));
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({'extended':'true'}));
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(methodOverride());
+
+var Todo = mongoose.model('Todo', {
+  text: String
+});
+
+// routes ======================================================================
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  console.log('a user connected');
+app.get('/api/todos', function(req, res) {
+    Todo.find(function(err, todos) {
+        if (err)
+            res.send(err)
+
+        res.json(todos);
+    });
 });
+
+app.post('/api/todos', function(req, res) {
+    console.log("creating new item");
+    Todo.create({
+        text : req.body.text,
+        done : false
+    }, function(err, todo) {
+        if (err)
+            res.send(err);
+        Todo.find(function(err, todos) {
+            if (err)
+                res.send(err)
+            res.json(todos);
+        });
+    });
+
+});
+
+app.delete('/api/todos/:todo_id', function(req, res) {
+    Todo.remove({
+        _id : req.params.todo_id
+    }, function(err, todo) {
+        if (err)
+            res.send(err);
+
+        Todo.find(function(err, todos) {
+            if (err)
+                res.send(err)
+            res.json(todos);
+        });
+    });
+});
+
+// socket.io ======================================================================
 
 io.on('connection', function(socket){
   console.log('a user connected');
---
+++ public/core.js
@@ -0,0 +1,39 @@
+var todoList = angular.module('todoList', []);
+
+function mainController($scope, $http) {
+    $scope.formData = {};
+
+    // get todolist items from db
+    $http.get('/api/todos')
+        .success(function(data) {
+            $scope.todos = data;
+            console.log(data);
+        })
+        .error(function(data) {
+            console.log('Error: ' + data);
+        });
+
+    $scope.createTodo = function() {
+        $http.post('/api/todos', $scope.formData)
+            .success(function(data) {
+                $scope.formData = {};
+                $scope.todos = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
+
+    $scope.deleteTodo = function(id) {
+        $http.delete('/api/todos/' + id)
+            .success(function(data) {
+                $scope.todos = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
+
+}
